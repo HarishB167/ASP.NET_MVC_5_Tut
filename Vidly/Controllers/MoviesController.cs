100996569e1df18ecd47167ed46fd7436a08284a
HarishB167 <harishbb168@gmail.com>
2022-06-02 19:21:05 +0530

    Section 2 : Action Parameters : completed

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -13,13 +13,23 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-            //return View(movie);
-            //return Content("Hello World");
-            //return HttpNotFound();
-            //return new EmptyResult();
-            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+            return View(movie);
         }
 
+        public ActionResult Edit(int movieId)
+        {
+            return Content("id=" + movieId);
+        }
 
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex{0}&sortBy={1}", pageIndex, sortBy));
+        }
     }
 }
