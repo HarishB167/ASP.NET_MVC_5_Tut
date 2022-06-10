3844ad1bb6490a5738361efc5bdc0d0da1e75a59
HarishB167 <harishbb168@gmail.com>
2022-06-10 23:42:32 +0530

    Section 10 : Exercise - Create API : completed

---
+++ Vidly/Dtos/RentalDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Dtos
+{
+    public class RentalDto
+    {
+        public string Customer { get; set; }
+        public IEnumerable<string> Movies { get; set; }
+    }
+}
