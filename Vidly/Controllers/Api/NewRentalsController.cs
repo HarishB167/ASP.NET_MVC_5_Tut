17e4fd1d6cecdcab51778ac464d916c0e9ad8fcc
HarishB167 <harishbb168@gmail.com>
2022-06-11 13:11:19 +0530

    Section 10 : Building the Simplest API : completed

--- Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -19,10 +19,31 @@ namespace Vidly.Controllers.Api
         }
 
         [HttpPost]
-        public IHttpActionResult CreateRental(NewRentalDto rentalDto)
+        public IHttpActionResult CreateNewRentals(NewRentalDto rentalDto)
         {
             if (!ModelState.IsValid)
-                return BadRequest();
+            {
+                var msg = String.Format("rentalDto : {0}", rentalDto);
+                return BadRequest(msg);
+            }
+
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == rentalDto.CustomerId);
+
+            foreach (var movieId in rentalDto.MovieIds)
+            {
+                var movie = _context.Movies.SingleOrDefault(m => m.Id == movieId);
+
+                Rental rental = new Rental();
+                rental.Customer = customer;
+                rental.CustomerId = customer.Id;
+                rental.Movie = movie;
+                rental.MovieId = movie.Id;
+                rental.DateRented = DateTime.Now;
+
+                _context.Rentals.Add(rental);
+            }
+
+            _context.SaveChanges();
             return Ok("Successfully added rental");
         }
     }
